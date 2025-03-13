8f0fc726e2a3441eba063ffb96c20c54e2d547d7
Leonard Mark Galivo <182197106+leonardmarkgalivo@users.noreply.github.com>
2025-03-13 22:57:32 +0800

    fixing_and_finalizing_batch_two

--- check_number_not_equal.py
+++ check_number_not_equal.py
@@ -1,11 +1,13 @@
+# START
+
 # Get input from the user
 num1 = float(input("First Number: "))  # Store first number
 num2 = float(input("Second Number: "))  # Store second number
 
-# Compare the numbers and determine the smallest
-if num1 > num2:
-    print("The smallest number is", num2)  # Print num2 if it's smaller
-elif num1 < num2:
-    print("The smallest number is", num1)  # Print num1 if it's smaller
+# Check if numbers are equal or not
+if num1 != num2:
+    print("Not Equal")
 else:
-    print("Both are equal")  # Print equal
+    print("Equal")
+
+# END
--- find_smallest_number.py
+++ find_smallest_number.py
@@ -1,14 +1,13 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 
-    # Get input from the user
-    num1 = float(input("First Number: "))
-    num2 = float(input("Second Number: "))
-
-    # Compare the numbers and determine the smallest
-    if num1 > num2:
-        print("The smallest number is", num2)
-    elif num1 < num2:
-        print("The smallest number is", num1)
-    else:
-        print("Both are equal")  # Print
+# Compare the numbers and determine the smallest
+if num1 > num2:
+    print("The smallest number is:", num2)
+elif num1 < num2:
+    print("The smallest number is:", num1)
+else:
+    print("Both numbers are equal.")
 
 
